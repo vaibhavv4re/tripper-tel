3374d860fb39af8b7855f3e74d544d70bb1b72d3
vaibhavv4re <vaibhavv4re@gmail.com>
2025-01-31 15:31:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 from database import save_preferences, get_preferences, log_interaction
 from transformers import pipeline
 import requests
